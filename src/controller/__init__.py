723a7075e083c0809b8726ffb7125e53bfd9bfb8
jbacon <julien.bacon.1@gmail.com>
2021-05-08 14:10:35 -0400

    scafollding done

--- src/controller/__init__.py
+++ src/controller/__init__.py
@@ -1,35 +1,29 @@
 import json
 import logging
 import os
-import pygame
 
-def list_controller():
-    """
-    List the connected controller.
-    """
+import inputs
 
 class Controller:
     """
     Class implementing the controller function.
     """
-
     CONFIG_ROOT_DIR = './src/controller/configs/'
 
-    def __init__(self, id, name, ndigit=2):
+    def __init__(self, idx, name, client):
         """
         Constructor.
 
         Params:
-            id:         The ID of the controller from 0 to pygame.joystick.get_count().
-            name:       The controller name.
-            ndigit:     The digit number for the axis precision. Default: 2.
+            idx:    The index of the controller in the list of connected gamepads.
+            name:   The controller name.
+            client: The MQTT client.
         """
-        logging.debug(f"creating controller {name} with id: {id}")
-        self._joystick = pygame.joystick.Joystick(id)
-        self._joystick.init()
+        logging.debug(f"creating controller {name} with at index {idx}.")
+        self._gamepad = inputs.devices.gamepads[idx]
 
-        self._id = id
-        self._ndigit = ndigit
+        self._idx = idx
+        self._client = client
 
         file_name = f"{name.lower().replace(' ', '_')}.json"
         config_file = os.path.join(self.CONFIG_ROOT_DIR, file_name)
@@ -39,62 +33,40 @@ class Controller:
     @classmethod
     def list_connected(cls):
         """
-        List the connected and supported controller.
+        List the connected controllers.
 
         Return:
-            A list containing the controller names. The index of the names correspond
-            to the controller ID.
+            A Dictionary containing the identification of the controller (index: name).
         """
-        logging.debug('listing connected/supported controller')
-        connected_controllers = []
-        for id in range(pygame.joystick.get_count()):
-            controller_name = pygame.joystick.Joystick(id).get_name()
-            connected_controllers.append(controller_name)
+        connected = []
+        for gamepad in inputs.devices.gamepads:
+            name = gamepad.__str__()
+            cut_idx = name.index(' ')
+            name = name[cut_idx+1:]
+            connected.append(name)
 
-        supported_controllers = [f.replace('.json', '') for f in os.listdir(cls.CONFIG_ROOT_DIR)]
-        conn_supp_controllers = filter(
-            lambda ctrl : any(ctrl.lower().replace(' ', '_') in ctrl_name for ctrl_name in supported_controllers),
-            connected_controllers)
-        conn_supp_controllers = tuple(conn_supp_controllers)
+        logging.debug(f"connected controller: {connected}")
 
-        logging.debug(f"found the following controllers: {conn_supp_controllers}")
+        supported = [f.replace('.json', '') for f in os.listdir(cls.CONFIG_ROOT_DIR)]
+        logging.debug(f"supported controller: {supported}")
 
-        return conn_supp_controllers
+        connected_supported = {}
+        for idx in range(len(connected)):
+            for supp in supported:
+                if supp == connected[idx].lower().replace(' ', '_'):
+                    connected_supported[idx] = connected[idx]
 
-    def get_name(self):
+        return connected_supported
+
+    def process_events(self):
         """
-        Get the joystick name.
-
-        Return:
-            The name of the controller.
+        Process the controller events.
         """
-        return self._joystick.get_name()
+        for event in self._gamepad.read():
+            logging.debug(f"processing event: {event.code}")
 
-    def get_id(self):
-        """
-        Get the joystick id
 
-        Return:
-            The ID of the controller.
-        """
-        return self._id
-
-    def get_buttons(self):
-        """
-        Get all the buttons state.
-
-        Return:
-            A tuple containing all the buttons state. 1 is pressed, 0 is depressed.
-        """
-        logging.debug(f"reading all button state of controller {self.get_name()}")
-        button_count = self._joystick.get_numbuttons()
-        button_states = [int(self._joystick.get_button(i)) for i in range(button_count)]
-
-        logging.debug(f"button states: {button_states}")
-
-        return (button_states)
-
-    def get_funct_mapping(self):
+    def _funct_mapping(self):
         """
         Get the buttons function mapping.
 
@@ -103,28 +75,21 @@ class Controller:
         """
         return self._config["mapping"]
 
-    def get_button_indexes(self):
+    def _button_events(self):
         """
-        Get the button indexes for the controller.
+        Get the button events for the controller.
 
         Return:
-            A dictionnary containing the controller button indexes mapping.
+            A dictionnary containing the controller button events mapping.
         """
         return self._config["buttons"]
 
-    def get_axis(self):
+    def _axis_events(self):
         """
-        Get all axis position.
+        Get all axis events for the controller.
 
         Return:
-            A tuple containing all the axis positions. The positions are ranging from -1 to 1.
+            A dictionnary containing the controller axis events mapping.
         """
-        logging.debug(f"reading all axis positions of controller {self.get_name()}")
-        axis_count = self._joystick.get_numaxes()
-        axis_positions = [float(self._joystick.get_axis(i)) for i in range(axis_count)]
-        axis_positions = [round(pos, self._ndigit) for pos in axis_positions]
-
-        logging.debug(f"axis positions: {axis_positions}")
-
-        return (axis_positions)
+        return self._config["axis"]
 
