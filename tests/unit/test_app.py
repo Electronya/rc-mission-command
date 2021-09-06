6cc8eae2ca9e780d91f83ed76bc9e88c59e24017
jbacon <julien.bacon.1@gmail.com>
2021-09-06 13:22:05 -0400

    adding dummy test

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -9,10 +9,15 @@ sys.path.append(os.path.abspath('./src'))
 mockedPygame = Mock()
 sys.modules['pygame'] = mockedPygame
 
-import app      # noqa: E402
+from app import App     # noqa: E402
 
 
 class TestApp(TestCase):
     """
     The app module test cases.
     """
+    def test_constructorPygame(self):
+        """
+        The constructor must init the pygame package.
+        """
+        pass
