9849c85968a8607b8bd4d1899c7ba65a2a2879f6
jbacon <julien.bacon.1@gmail.com>
2021-09-08 11:33:17 -0400

    rework + test constructor

--- src/app.py
+++ src/app.py
@@ -8,88 +8,113 @@ import pygame
 
 from logger import initLogger
 from pkgs.controller import Controller
-from pkgs.client import Client
+import pkgs.mqttClient as client
 from ui.baseFrame import BaseFrame
 from pkgs.unit import Unit
-
-pygame.init()
-pygame.event.set_allowed([pygame.JOYAXISMOTION, pygame.JOYBUTTONDOWN,
-                          pygame.JOYBUTTONUP, pygame.JOYHATMOTION])
-appLogger = initLogger()
 
 
 class App(tk.Tk):
     """
     The application base class.
     """
+    WINDOW_TITLE = 'RC Mission Commander'
+    CLIENT_ID = 'commander'
+    CLIENT_PASSWD = '12345'
     CTRL_FRAME_RATE = 10
 
-    def __init__(self, ctrlsNameList):
+    def __init__(self):
         """
         Constructor.
-
-        Params:
-            ctrlrNameList       The list of connected controller names.
         """
         tk.Tk.__init__(self)
-
-        self._units = {'active': None, 'list': []}
-
-        self.title('RC Mission Commander')
-        self._logger = appLogger.getLogger('APP')
-        self._logger.info('launcihing application...')
-
-        self._logger.info('initializing mqtt client.')
-        self._client = Client(self, '12345')
-        self._logger.info('mqtt client initialized.')
-
-        self._logger.info('initializing controller.')
-        self._init_controllers(ctrlsNameList)
-        self._logger.info('controller initialzed.')
-
-        self._logger.info('initializing UI.')
-        self.attributes('-zoomed', True)
-
-        style = ttk.Style()
-        style.theme_use('clam')
-        style.configure("red.Horizontal.TProgressbar",
-                        foreground='red', background='red')
-        style.configure("grn.Horizontal.TProgressbar",
-                        foreground='green', background='green')
-
-        self._baseFrame = BaseFrame(self, self._controllers, self._units)
-        self._baseFrame.pack(fill=tk.BOTH, expand=True)
-        self._logger.info('UI initialized.')
-
+        appLogger = self._initLogger()
+        self._initPygame()
+        self._initMqttClient()
+        self._initControllers(appLogger)
+        self._initUsrInterface()
         self.after(Controller.CTRL_FRAME_RATE, self._process_pygame_events)
+        # self._units = {'active': None, 'list': []}
+        # self.title('RC Mission Commander')
+        # appLogger = initLogger()
+        # self._logger = appLogger.getLogger('APP')
+        # self._logger.info('launcihing application...')
+        # self._logger.info('initializing pygame...')
+        # pygame.init()
+        # pygame.event.set_allowed([pygame.JOYAXISMOTION, pygame.JOYBUTTONDOWN,
+        #                           pygame.JOYBUTTONUP, pygame.JOYHATMOTION])
+        # self._logger.info('pygame initialized')
+        # self._logger.info('initializing mqtt client...')
+        # client.init(appLogger, self.CLIENT_ID, self.CLIENT_PASSWD)
+        # self._logger.info('mqtt client initialized')
+        # self._logger.info('initializing controller...')
+        # self._init_controllers(ctrlsNameList)
+        # self._logger.info('controller initialzed.')
 
-        self._logger.info('application launched.')
+        # self._logger.info('initializing UI.')
+        # self.attributes('-zoomed', True)
 
-    def quit(self):
+        # style = ttk.Style()
+        # style.theme_use('clam')
+        # style.configure("red.Horizontal.TProgressbar",
+        #                 foreground='red', background='red')
+        # style.configure("grn.Horizontal.TProgressbar",
+        #                 foreground='green', background='green')
+
+        # self._baseFrame = BaseFrame(self, self._controllers, self._units)
+        # self._baseFrame.pack(fill=tk.BOTH, expand=True)
+        # self._logger.info('UI initialized.')
+
+        # self._logger.info('application launched.')
+
+    def _initLogger(self) -> object:
         """
-        Quit the application.
-        """
-        self._logger.info('quitting the application')
-        for controller in self._controllers['list']:
-            controller.quit()
-        pygame.quit()
-        self._client.disconnect()
-        self.destroy()
-        sys.exit()
+        Initialize the application logger.
 
-    def _init_controllers(self, ctrlrNameList):
+        Return:
+            The initialized application logger.
+        """
+        pass
+
+    def _initPygame(self) -> None:
+        """
+        Initialize the pygame pakage.
+        """
+        pass
+
+    def _initMqttClient(self) -> None:
+        """
+        Initialize the MQTT client.
+        """
+        pass
+
+    def _listControllers(self) -> tuple:
+        """
+        List the available controllers.
+
+        Return:
+            The list of available controllers.
+        """
+        pass
+
+    def _initControllers(self, appLogger: object) -> None:
         """
         Initialize the controllers.
 
         Params:
-            ctrlrNameList:      The list of connected controller names.
+            appLogger:      The application logger.
         """
-        self._controllers = {}
-        controllers = []
-        for ctrl_name in ctrlrNameList.keys():
-            controllers.append(Controller(ctrlrNameList[ctrl_name],
-                                          ctrl_name, self))
-        self._controllers = {'active': controllers[0], 'list': controllers}
+        # self._controllers = {}
+        # controllers = []
+        # for ctrl_name in ctrlrNameList.keys():
+        #     controllers.append(Controller(ctrlrNameList[ctrl_name],
+        #                                   ctrl_name, self))
+        # self._controllers = {'active': controllers[0], 'list': controllers}
+
+    def _initUsrInterface(self) -> None:
+        """
+        Initialize the user interface.
+        """
+        pass
 
     def _process_pygame_events(self):
         """
@@ -136,6 +161,18 @@ class App(tk.Tk):
                 unitFunctions = self._units['active'].get_functions()
                 unitFunctions[ctrlrFunctions[axis[axisIdx]]](modifier)
 
+    def quit(self):
+        """
+        Quit the application.
+        """
+        self._logger.info('quitting the application')
+        for controller in self._controllers['list']:
+            controller.quit()
+        pygame.quit()
+        self._client.disconnect()
+        self.destroy()
+        sys.exit()
+
     def add_unit(self, unitId):
         """
         Add a new unit.
@@ -166,7 +203,7 @@ def list_connected_controllers():
     List the connected controllers.
     """
     controllerNames = Controller.listController()
-    appLogger.debug(f"controller list: {controllerNames}")
+    # appLogger.debug(f"controller list: {controllerNames}")
     if len(controllerNames):
         return controllerNames
 
--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -16,8 +16,102 @@ class TestApp(TestCase):
     """
     The app module test cases.
     """
-    def test_constructorPygame(self):
+    def setUp(self):
         """
-        The constructor must init the pygame package.
+        Test cases setup.
         """
-        pass
+        self.testLogger = Mock()
+
+    @patch('app.tk.Tk.__init__')
+    def test_constructorInitTk(self, mockedTkInit):
+        """
+        The constructor must initialize the Tk class.
+        """
+        with patch.object(App, '_initLogger'), \
+                patch.object(App, '_initPygame'), \
+                patch.object(App, '_initMqttClient'), \
+                patch.object(App, '_initControllers'), \
+                patch.object(App, '_initUsrInterface'), \
+                patch.object(App, 'after'):
+            testApp = App()
+            mockedTkInit.assert_called_once_with(testApp)
+
+    def test_contructorInitLogger(self):
+        """
+        The constructor must initialize the application logger.
+        """
+        with patch.object(App, '_initLogger') as mockedInitLog, \
+                patch.object(App, '_initPygame'), \
+                patch.object(App, '_initMqttClient'), \
+                patch.object(App, '_initControllers'), \
+                patch.object(App, '_initUsrInterface'), \
+                patch.object(App, 'after'):
+            testApp = App()     # noqa: F841
+            mockedInitLog.assert_called_once()
+
+    def test_constructorInitPygame(self):
+        """
+        The constructor must initialize the pygame pakage.
+        """
+        with patch.object(App, '_initLogger'), \
+                patch.object(App, '_initPygame') as mockedInitPygame, \
+                patch.object(App, '_initMqttClient'), \
+                patch.object(App, '_initControllers'), \
+                patch.object(App, '_initUsrInterface'), \
+                patch.object(App, 'after'):
+            testApp = App()     # noqa: F841
+            mockedInitPygame.assert_called_once()
+
+    def test_constructorInitMqttClient(self):
+        """
+        The constructor must initialize the MQTT client.
+        """
+        with patch.object(App, '_initLogger'), \
+                patch.object(App, '_initPygame'), \
+                patch.object(App, '_initMqttClient') as mockedInitClient, \
+                patch.object(App, '_initControllers'), \
+                patch.object(App, '_initUsrInterface'), \
+                patch.object(App, 'after'):
+            testApp = App()     # noqa: F841
+            mockedInitClient.assert_called_once()
+
+    def test_constructorInitControllers(self):
+        """
+        The constructor must initialize the controllers.
+        """
+        with patch.object(App, '_initLogger') as mockedInitLog, \
+                patch.object(App, '_initPygame'), \
+                patch.object(App, '_initMqttClient'), \
+                patch.object(App, '_initControllers') as mockedInitCtrls, \
+                patch.object(App, '_initUsrInterface'), \
+                patch.object(App, 'after'):
+            mockedInitLog.return_value = self.testLogger
+            testApp = App()     # noqa: F841
+            mockedInitCtrls.assert_called_once_with(self.testLogger)
+
+    def test_constructorInitUsrInterface(self):
+        """
+        The constructor must initialize the user interface.
+        """
+        with patch.object(App, '_initLogger'), \
+                patch.object(App, '_initPygame'), \
+                patch.object(App, '_initMqttClient'), \
+                patch.object(App, '_initControllers'), \
+                patch.object(App, '_initUsrInterface') as mockedInitUI, \
+                patch.object(App, 'after'):
+            testApp = App()     # noqa: F841
+            mockedInitUI.assert_called_once()
+
+    def test_constructorStartEventLoop(self):
+        """
+        The constructor must start the event loop.
+        """
+        with patch.object(App, '_initLogger'), \
+                patch.object(App, '_initPygame'), \
+                patch.object(App, '_initMqttClient'), \
+                patch.object(App, '_initControllers'), \
+                patch.object(App, '_initUsrInterface'), \
+                patch.object(App, 'after') as mockedAfter:
+            testApp = App()
+            mockedAfter.assert_called_once_with(testApp.CTRL_FRAME_RATE,
+                                                testApp._process_pygame_events)
