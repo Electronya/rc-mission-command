f7b908b9ca65d65a69da36a520afc9960900d152
jbacon <julien.baccon.1@gmail.com>
2022-02-20 09:21:33 -0500

    adding axis motion callback

--- src/pkgs/ui/controllers/joystickCtrlr/joystickCtrlr.py
+++ src/pkgs/ui/controllers/joystickCtrlr/joystickCtrlr.py
@@ -1,5 +1,5 @@
-
 from PySide2.QtCore import QObject, Signal, Slot
+from PySide2.QtGui import QTransform
 from PySide2.QtSvg import QGraphicsSvgItem
 from PySide2.QtWidgets import QComboBox, QGraphicsView, \
     QGraphicsScene, QMessageBox, QProgressBar, QPushButton
@@ -42,7 +42,8 @@ class JoystickCtrlr(QObject):
         self._thrtlBar = thrtlBar
         self._brakeBar = brkBar
         self._model = JoystickModel(logger)
-        self._model.calibration.connect(self.createCalibMsgBox)
+        self._model.calibration.connect(self._createCalibMsgBox)
+        self._model.axisMotion.connect(self._axisMotionCallback)
         self._initWidgets()
         self._logger.info('intialized')
 
@@ -53,7 +54,7 @@ class JoystickCtrlr(QObject):
         self._calBtn.clicked.connect(self._model.calibrateJoystick)
         self._selectCombo.setModel(self._model.model)
         self._selectCombo.currentTextChanged \
-            .connect(self._model.activateJoystick)
+            .connect(self._switchJoystick)
         self._initWheelWidgets()
         self._thrtlBar.setValue(0)
         self._thrtlBar.setStyleSheet(self.THRTL_STYLESHEET)
@@ -70,20 +71,8 @@ class JoystickCtrlr(QObject):
         scene.addItem(icon)
         self._wheelView.setScene(scene)
 
-    def areJoystickAvailable(self) -> None:
-        """
-        Check if there is an available joystick.
-        """
-        availableCount = self._model.model.rowCount()
-        if availableCount == 0:
-            self._logger.error(f"available joystick count: {availableCount}")
-            self.error.emit(QMessageBox.Critical,
-                            Exception('No joystick are available.'))
-        else:
-            self._calBtn.setEnabled(not self._model.isJoystickCalibrated())
-
     @Slot(str)
-    def createCalibMsgBox(self, msg: str) -> None:
+    def _createCalibMsgBox(self, msg: str) -> None:
         """
         Create the calibration message box.
         """
@@ -96,3 +85,74 @@ class JoystickCtrlr(QObject):
             self._calBtn.setEnabled(False)
         else:
             self._model.calibrateJoystick()
+
+    @Slot(str)
+    def _switchJoystick(self, joystickName: str) -> None:
+        """
+        Switch between connected joystick.
+        """
+        self._model.activateJoystick(joystickName)
+        self._calBtn.setEnabled(not self._model.isJoystickCalibrated())
+
+    def _updateSteering(self, modifier: float) -> None:
+        """
+        Update the steering wheel widget.
+
+        Params:
+            modifier:   The steering modifier.
+        """
+        transform = QTransform()
+        transform.rotate(180 * modifier)
+        self._wheelView.setTransform(transform)
+
+    def _updateThrottle(self, modifier: float) -> None:
+        """
+        Update the throttle widget.
+
+        Params:
+            modifier:   The throttle modifier.
+        """
+        minimum = self._thrtlBar.minimum()
+        thrtlRange = self._thrtlBar.maximum() - minimum
+        self._thrtlBar.setValue(int((thrtlRange * modifier) + minimum))
+
+    def _updateBrake(self, modifier: float) -> None:
+        """
+        Update the brake widget.
+
+        Params:
+            modifier:   The brake modifier.
+        """
+        minimum = self._brakeBar.minimum()
+        brakeRange = self._brakeBar.maximum() - minimum
+        self._brakeBar.setValue(int((brakeRange * modifier) + minimum))
+
+    @Slot(str, int, float)
+    def _axisMotionCallback(self, type: str,
+                            idx: int, modifier: float) -> None:
+        """
+        Callback for axis motion.
+
+        Params:
+            type:       The joystick type.
+            idx:        The axis index.
+            modifier:   The axis modifier.
+        """
+        updateSwitch = (
+            self._updateSteering,
+            self._updateThrottle,
+            self._updateBrake
+        )
+        updateSwitch[idx](modifier)
+
+    def areJoystickAvailable(self) -> None:
+        """
+        Check if there is an available joystick.
+        """
+        availableCount = self._model.model.rowCount()
+        if availableCount == 0:
+            self._logger.error(f"available joystick count: {availableCount}")
+            self.error.emit(QMessageBox.Critical,
+                            Exception('No joystick are available.'))
+        else:
+            self._calBtn.setEnabled(not self._model.isJoystickCalibrated())
--- tests/unit/pkgs/ui/controllers/joystickCtrlr/test_JoystickCtrlr.py
+++ tests/unit/pkgs/ui/controllers/joystickCtrlr/test_JoystickCtrlr.py
@@ -22,6 +22,7 @@ class TestJoystickCtrlr(TestCase):
         self.graphicScene = 'pkgs.ui.controllers.joystickCtrlr.joystickCtrlr.QGraphicsScene'        # noqa: E501
         self.graphSvgItem = 'pkgs.ui.controllers.joystickCtrlr.joystickCtrlr.QGraphicsSvgItem'      # noqa: E501
         self.messageBox = 'pkgs.ui.controllers.joystickCtrlr.joystickCtrlr.QMessageBox'             # noqa: E501
+        self.transform = 'pkgs.ui.controllers.joystickCtrlr.joystickCtrlr.QTransform'               # noqa: E501
         self.mockedLogger = Mock()
         self.mockedJoystickModel = Mock()
         self.mockedGraphScene = Mock()
@@ -63,6 +64,7 @@ class TestJoystickCtrlr(TestCase):
                           self.mockedThrtlBar, self.mockedBrkBar)
             mockedCtrlrMdl.assert_called_once_with(self.mockedLogger)
             mockedModel.calibration.connect.assert_called_once()
+            mockedModel.axisMotion.connect.assert_called_once()
 
     def test_constructorInitWidgets(self):
         """
@@ -100,7 +102,7 @@ class TestJoystickCtrlr(TestCase):
             self.mockedJoystickSelect.setModel. \
                 assert_called_once_with(self.mockedJoystickModel.model)
             self.mockedJoystickSelect.currentTextChanged.connect. \
-                assert_called_once_with(self.mockedJoystickModel.activateJoystick)      # noqa: E501
+                assert_called_once_with(self.joystickCtrlr._switchJoystick)
 
     def test_initWidgetsWheel(self):
         """
@@ -145,9 +147,138 @@ class TestJoystickCtrlr(TestCase):
             self.mockedWheelView.setScene. \
                 assert_called_once_with(self.mockedGraphScene)
 
+    def test_createCalibMsgBoxCreateMsgBx(self):
+        """
+        The _createCalibMsgBox method must create and display
+        the calibration message box with the received message.
+        """
+        expectedMsg = 'test message'
+        mockedMsgBox = Mock()
+        with patch(self.messageBox) as mockedMsgBoxConst:
+            mockedMsgBoxConst.return_value = mockedMsgBox
+            self.joystickCtrlr._createCalibMsgBox(expectedMsg)
+            mockedMsgBoxConst.assert_called_once()
+            mockedMsgBox.setWindowTitle.assert_called_once()
+            mockedMsgBox.setText.assert_called_once_with(expectedMsg)
+            mockedMsgBox.setIcon.assert_called_once()
+            mockedMsgBox.exec_.assert_called_once()
+
+    def test_createCalibMsgBoxNextSequence(self):
+        """
+        The _createCalibMsgBox method must go to the next
+        calibration sequence.
+        """
+        with patch(self.messageBox):
+            self.joystickCtrlr._createCalibMsgBox('TEST')
+            self.mockedJoystickModel.calibrateJoystick.assert_called_once()
+
+    def test_createCalibMsgBoxDone(self):
+        """
+        The _createCalibMsgBox method must disable the calibration
+        button if the calibration process is done.
+        """
+        with patch(self.messageBox):
+            self.joystickCtrlr._createCalibMsgBox('hdhdh done oofid')
+            self.mockedCalBtn.setEnabled.assert_called_once_with(False)
+
+    def test_switchJoystickActivateJoystick(self):
+        """
+        The _switchJoystick method must activate the newly
+        selected joystick.
+        """
+        newJoystick = 'new joystick'
+        self.joystickCtrlr._switchJoystick(newJoystick)
+        self.mockedJoystickModel.activateJoystick \
+            .assert_called_once_with(newJoystick)
+
+    def test_switchJoystickCalibBtnEnState(self):
+        """
+        The _switchJoystick method must set the calibration button
+        enable state base on the newly activated joystick
+        calibration state.
+        """
+        newJoystick = 'new joystick'
+        calibStates = (True, False)
+        self.mockedJoystickModel.isJoystickCalibrated.side_effect = calibStates
+        for calibState in calibStates:
+            self.joystickCtrlr._switchJoystick(newJoystick)
+            self.mockedCalBtn.setEnabled \
+                .assert_called_with(not calibState)
+
+    def test_updateSteering(self):
+        """
+        The _updateSteering method must update the wheel widget
+        transform.
+        """
+        testMods = (-1.0, -0.75, 0.0, 0.34, 1.0)
+        mockedTransform = Mock()
+        for testMod in testMods:
+            with patch(self.transform) as mockedTransformConst:
+                mockedTransformConst.return_value = mockedTransform
+                self.joystickCtrlr._updateSteering(testMod)
+                mockedTransformConst.assert_called_once()
+                mockedTransform.rotate.assert_called_with(180 * testMod)
+                self.mockedWheelView.setTransform \
+                    .assert_called_with(mockedTransform)
+
+    def test_updateThrottle(self):
+        """
+        The _updateThrottle method must update the throttle widget.
+        """
+        testMods = (0.0, 0.26, 0.68, 1.0)
+        testMin = 0
+        testMax = 100
+        testRange = testMax - testMin
+        for testMod in testMods:
+            self.mockedThrtlBar.minimum.return_value = testMin
+            self.mockedThrtlBar.maximum.return_value = testMax
+            self.joystickCtrlr._updateThrottle(testMod)
+            self.mockedThrtlBar.setValue \
+                .assert_called_with(int((testRange * testMod) + testMin))
+
+    def test_updateBrake(self):
+        """
+        The _updateBrake method must update the brake widget.
+        """
+        testMods = (0.0, 0.26, 0.68, 1.0)
+        testMin = 0
+        testMax = 100
+        testRange = testMax - testMin
+        for testMod in testMods:
+            self.mockedBrkBar.minimum.return_value = testMin
+            self.mockedBrkBar.maximum.return_value = testMax
+            self.joystickCtrlr._updateBrake(testMod)
+            self.mockedBrkBar.setValue \
+                .assert_called_with(int((testRange * testMod) + testMin))
+
+    def test_axisMotionCallbackUpdateWidgets(self):
+        """
+        The _axisMotionCallback method must update the widget link the
+        axis being moved.
+        """
+        testVals = (
+            {'type': 'steering wheel', 'axis': 0, 'mod': -0.25},
+            {'type': 'steering wheel', 'axis': 1, 'mod': 0.49},
+            {'type': 'steering wheel', 'axis': 2, 'mod': 0.82},
+        )
+        with patch.object(self.joystickCtrlr, '_updateSteering') \
+                as mockedUpdateSteering, \
+                patch.object(self.joystickCtrlr, '_updateThrottle') \
+                as mockedUpdateThrtl, \
+                patch.object(self.joystickCtrlr, '_updateBrake') \
+                as mockedUpdateBrake:
+            testVals[0]['update'] = mockedUpdateSteering
+            testVals[1]['update'] = mockedUpdateThrtl
+            testVals[2]['update'] = mockedUpdateBrake
+            for testVal in testVals:
+                self.joystickCtrlr._axisMotionCallback(testVal['type'],
+                                                       testVal['axis'],
+                                                       testVal['mod'])
+                testVal['update'].assert_called_once_with(testVal['mod'])
+
     def test_areJoystickAvailableNoJoystick(self):
         """
-        _areJoystickAvailable method must emit an error signal
+        The _areJoystickAvailable method must emit an error signal
         only if there are no available joystick and set the
         calibration button enabled state base on the calibration
         state of the active joystick.
@@ -162,37 +293,3 @@ class TestJoystickCtrlr(TestCase):
                 self.joystickCtrlr.areJoystickAvailable()
             mockedErrSignal.emit.assert_called_once()
             self.mockedCalBtn.setEnabled.assert_called_once_with(not False)
-
-    def test_createCalibMsgBoxCreateMsgBx(self):
-        """
-        The createCalibMsgBox method must create and display
-        the calibration message box with the received message.
-        """
-        expectedMsg = 'test message'
-        mockedMsgBox = Mock()
-        with patch(self.messageBox) as mockedMsgBoxConst:
-            mockedMsgBoxConst.return_value = mockedMsgBox
-            self.joystickCtrlr.createCalibMsgBox(expectedMsg)
-            mockedMsgBoxConst.assert_called_once()
-            mockedMsgBox.setWindowTitle.assert_called_once()
-            mockedMsgBox.setText.assert_called_once_with(expectedMsg)
-            mockedMsgBox.setIcon.assert_called_once()
-            mockedMsgBox.exec_.assert_called_once()
-
-    def test_createCalibMsgBoxNextSequence(self):
-        """
-        The createCalibMsgBox method must go to the next
-        calibration sequence.
-        """
-        with patch(self.messageBox):
-            self.joystickCtrlr.createCalibMsgBox('TEST')
-            self.mockedJoystickModel.calibrateJoystick.assert_called_once()
-
-    def test_createCalibMsgBoxDone(self):
-        """
-        The createCalibMsgBox method must disable the calibration
-        button if the calibration process is done.
-        """
-        with patch(self.messageBox):
-            self.joystickCtrlr.createCalibMsgBox('hdhdh done oofid')
-            self.mockedCalBtn.setEnabled.assert_called_once_with(False)
